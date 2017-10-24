07db6b092401d364ce71b4bd8c9c243be1efa0a7
Miguel Silva <msilva0897@gmail.com>
2017-10-24 00:41:17 -0600

    finish the admin controllers

--- app/controllers/admin/ideas_controller.rb
+++ app/controllers/admin/ideas_controller.rb
@@ -1,2 +1,11 @@
 class Admin::IdeasController < ApplicationController
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = current_user
+    @idea = Idea.find(params[:id])
+  end
 end
--- app/controllers/admin/images_controller.rb
+++ app/controllers/admin/images_controller.rb
@@ -1,2 +1,47 @@
 class Admin::ImagesController < ApplicationController
+  def index
+    @images = Image.all
+  end
+
+  def show
+    @image = Image.find(params[:id])
+  end
+
+  def new
+    @image = Image.new
+  end
+
+  def create
+    @image = Image.new(image_params)
+    if @image.save
+      redirect_to admin_image_path(@image)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @image = Image.find(params[:id])
+  end
+
+  def update
+    @image = Image.find(params[:id])
+    if @image.update(image_params)
+      redirect_to admin_image_path(@image)
+    else
+      redirect_to request.referrer
+    end
+  end
+
+  def destroy
+    image = Image.find(params[:id])
+    image.destroy
+    redirect_to admin_images_path
+  end
+
+  private
+
+  def image_params
+    params.require(:image).permit(:image, :name, idea_ids: [])
+  end
 end
