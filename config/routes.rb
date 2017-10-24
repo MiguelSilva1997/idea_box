4caaf272d643e758f758621d29bc484d3a8a22a5
Miguel Silva <msilva0897@gmail.com>
2017-10-23 21:22:52 -0600

    create routes for users and sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :user do
+    resources :ideas
+  end
+
+  namespace :admin do
+    resources :categories
+    resources :images
+    resources :ideas
+  end
+
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to "sessions#destroy"
 end
