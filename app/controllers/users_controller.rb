faac409ba12675aa770f6f6338645d7a67b5badc
Miguel Silva <msilva0897@gmail.com>
2017-10-23 21:15:31 -0600

    create new and create verbs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,29 @@
 class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @user = User.find(session[:user_id])
+    if @user.nil?
+      redirect_to login_path
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :username, :password)
+  end
 end
